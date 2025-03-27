e3c730a93943e2f99b01d8743e85c2c76119f17d
shulaoda <laipichan@qq.com>
2025-03-27 18:44:24 +0800

    save

---
+++ benches/bench.rs
@@ -0,0 +1,54 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use phf::phf_set;
+
+const DISALLOW_NEW_FOR_BUILTINS_1: [&str; 12] = [
+    "BigInt", "Boolean", "Number", "adwad", "dawgfaw", " HHAWD", "awf", "5169", "2dwqqd", "dwqf",
+    "Symbol", "String",
+];
+
+const DISALLOW_NEW_FOR_BUILTINS_2: phf::Set<&'static str> = phf_set! {
+    "BigInt",
+    "Boolean",
+    "Number",
+    "adwad", "dawgfaw", " HHAWD", "awf", "5169", "2dwqqd", "dwqf",
+    "Symbol",
+    "String",
+};
+
+fn phf_bad() {
+    let _ = DISALLOW_NEW_FOR_BUILTINS_2.contains("Stringa");
+}
+
+fn phf_first() {
+    let _ = DISALLOW_NEW_FOR_BUILTINS_2.contains("BigInt");
+}
+
+fn phf_last() {
+    let _ = DISALLOW_NEW_FOR_BUILTINS_2.contains("String");
+}
+
+fn array_bad() {
+    let _ = DISALLOW_NEW_FOR_BUILTINS_1.contains(&"Stringa");
+}
+
+fn array_first() {
+    let _ = DISALLOW_NEW_FOR_BUILTINS_1.contains(&"BigInt");
+}
+
+fn array_last() {
+    let _ = DISALLOW_NEW_FOR_BUILTINS_1.contains(&"String");
+}
+
+fn benchmark(c: &mut Criterion) {
+    c.bench_function("phf bad", |b| b.iter(|| phf_bad()));
+    c.bench_function("array bad", |b| b.iter(|| array_bad()));
+
+    c.bench_function("phf first", |b| b.iter(|| phf_first()));
+    c.bench_function("array first", |b| b.iter(|| array_first()));
+
+    c.bench_function("phf last", |b| b.iter(|| phf_last()));
+    c.bench_function("array last", |b| b.iter(|| array_last()));
+}
+
+criterion_group!(benches, benchmark);
+criterion_main!(benches);
